7c30ee594adcb8a8965d764085b1262ef6a2d6d4
Saeed Gholami <saeed.ghollami@gmail.com>
2023-03-18 21:55:19 +0330

    Add multiple images for a product.

--- shop_project/store/admin.py
+++ shop_project/store/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 
-from .models import Category, Product
-
+from .models import Category, Product, ProductImage
 
 
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(ProductImage)
+
+
+class ImageInline(admin.TabularInline):
+    model = ProductImage
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ImageInline,
+    ]
--- shop_project/store/models.py
+++ shop_project/store/models.py
@@ -8,10 +8,16 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
-
     def __str__(self):
         return str(self.name)
-    
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(
+        "Product", related_name="images", on_delete=models.CASCADE
+    )
+    image = models.ImageField(upload_to="images/")
+
 
 class Product(models.Model):
     title = models.CharField(max_length=250)
@@ -19,9 +25,6 @@ class Product(models.Model):
     brand = models.CharField(max_length=150, default="un-branded")
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to="images/")
 
     def __str__(self):
         return str(self.title)
-
-
